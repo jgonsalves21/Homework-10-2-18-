48e73ecf10861af9d89cd845f9fc991e9f340a75
jgonsalves21 <42977061+jgonsalves21@users.noreply.github.com>
2018-10-02 13:37:35 -0400

    Add files via upload

---
+++ src/Input.java
@@ -0,0 +1,22 @@
+
+	import java.util.Scanner;
+
+public class Input {
+
+	
+	
+	public static void main(String[] args) 
+	{
+		
+		System.out.print("Please enter the length of the rectangle: ");
+		double length = in.nextDouble();
+		
+		System.out.print("Please eenter the width);
+		
+
+		
+		
+		
+	}
+
+}
